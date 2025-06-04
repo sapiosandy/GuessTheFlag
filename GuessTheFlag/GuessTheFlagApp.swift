aacf30ec13ae2154c747a798ead6e4b53cd02ac9
Sandy Gomez <sgomez1029@gmail.com>
2025-06-04 09:22:45 -0700

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Sandra Gomez on 6/4/25.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
