af55f628659286ca4ab9523bdc2ee94c24d9c122
Sandy Gomez <sgomez1029@gmail.com>
2025-06-04 14:14:41 -0700

    Day 20, Part One - Showing alert messages

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -7,46 +7,40 @@
 
 import SwiftUI
 
+// Here's some example code that shows an alert when a button is tapped:
+
 struct ContentView: View {
+    @State private var showingAlert = false
     
     var body: some View {
-        // SwiftUI has a dedicated Image type for handling pictures in your apps, and there are three main ways you will create them:
+        Button("Show Alert") {
+            showingAlert = true
+        }
+//        .alert("Important message", isPresented: $showingAlert) {
+//            Button("OK") {}
+//        }
+//        
+        //The first part is the alert title, which is straightforward enough, but there's also another two-way data binding because SwiftUI will automatically set showingAlert back to false when the alert is dismissed.
+        // Now look at the button:
+        // Button("Ok") {}
+        // That's an empty closure, meaning that we aren't assigning any functionality to run when the button is pressed That doesn't matter though because any button inside an alert will automatically dismiss the alert - that closure is there to let us add any extra functionality beyond just dismissing the alert.
         
-       // Image("pencil") will load an image called “Pencil” that you have added to your project.
-       // Image(decorative: "pencil") will load the same image, but won’t read it out for users who have enabled the screen reader. This is useful for images that don’t convey additional important information.
-        //Image(systemName: "pencil") will load the pencil icon that is built into iOS. This uses Apple’s SF Symbols icon collection, and you can search for icons you like – download Apple’s free SF Symbols app from the web to see the full set.
+        // You can add more buttons to your alert, and this is a particularly good place to add roles to make sure it's clear what each button does.
         
-        // By default the screen reader will read your image name if it is enabled, so make sure you give your images clear names if you want to avoid confusing the user. Or, if they don’t actually add information that isn’t already elsewhere on the screen, use the Image(decorative:) initializer.
-        
-        //Because the longer form of buttons can have any kind of views inside them, you can use images like this:
-
-//        Button {
-//            print("Edit button was tapped")
-//        } label: {
-//            Image(systemName: "pencil")
+//        .alert("Important message", isPresented: $showingAlert) {
+//            Button("Delete", role: .destructive) { }
+//            Button("Cancel", role: .cancel) { }
 //        }
         
-        //If you want both text and image at the same time, you have two options. The first is provide them both to the Button directly, like this:
-        
-//        Button("Edit", systemImage: "pencil") {
-//            print("Edit button was tapped")
-//        }
-        
-        //But if you want something more custom, SwiftUI has a dedicated type called Label.
-
-        Button {
-            print("Edit button was tapped")
-        } label: {
-            Label("Edit", systemImage: "pencil")
-                .padding()
-                .foregroundStyle(.white)
-                .background(.red)
+        // And finally you can add message text to go alongside your title with a second trailing closure like this:
+        .alert("Important message", isPresented: $showingAlert) {
+            Button("OK", role: .cancel) { }
+        } message: {
+            Text("Please read this.")
         }
     }
-    
-    // Both of those will show both a pencil icon and the word “Edit” side by side, which on the surface sounds exactly the same as what we’d get by using a simple HStack. However, SwiftUI is really smart: it will automatically decide whether to show the icon, the text, or both depending on how they are being used in our layout, which makes this option a great choice.
-  
 }
+    
 
 
 #Preview {
