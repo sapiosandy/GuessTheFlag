929474a67b15db48fb5c07b0bfea1c797a78ba21
Sandy Gomez <sgomez1029@gmail.com>
2025-06-09 09:09:20 -0700

    Day 21, Part Two - Showing the Player's Score With an Alert

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -7,40 +7,57 @@
 
 import SwiftUI
 
-// Here's some example code that shows an alert when a button is tapped:
-
 struct ContentView: View {
-    @State private var showingAlert = false
+    
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
+    
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    
     
     var body: some View {
-        Button("Show Alert") {
-            showingAlert = true
+        ZStack {
+            Color.blue
+                .ignoresSafeArea()
+            VStack(spacing: 30) {
+                VStack {
+                    Text("Tap the flag of")
+                        .foregroundStyle(.white)
+                    Text(countries[correctAnswer])
+                        .foregroundStyle(.white)
+                }
+                
+                ForEach(0..<3) { number in
+                    Button {
+                        flagTapped(number)
+                    } label : {
+                        Image(countries[number])
+                    }
+                }
+            }
         }
-//        .alert("Important message", isPresented: $showingAlert) {
-//            Button("OK") {}
-//        }
-//        
-        //The first part is the alert title, which is straightforward enough, but there's also another two-way data binding because SwiftUI will automatically set showingAlert back to false when the alert is dismissed.
-        // Now look at the button:
-        // Button("Ok") {}
-        // That's an empty closure, meaning that we aren't assigning any functionality to run when the button is pressed That doesn't matter though because any button inside an alert will automatically dismiss the alert - that closure is there to let us add any extra functionality beyond just dismissing the alert.
-        
-        // You can add more buttons to your alert, and this is a particularly good place to add roles to make sure it's clear what each button does.
-        
-//        .alert("Important message", isPresented: $showingAlert) {
-//            Button("Delete", role: .destructive) { }
-//            Button("Cancel", role: .cancel) { }
-//        }
-        
-        // And finally you can add message text to go alongside your title with a second trailing closure like this:
-        .alert("Important message", isPresented: $showingAlert) {
-            Button("OK", role: .cancel) { }
-        } message: {
-            Text("Please read this.")
+        .alert(scoreTitle, isPresented: $showingScore) {
+            Button("Continue", action: askQuestion)
+        } message : {
+            Text("Your score is ???")
         }
     }
-}
     
+    func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+        } else {
+            scoreTitle = "Wrong"
+        }
+        showingScore = true
+    }
+
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+    }
+}
 
 
 #Preview {
